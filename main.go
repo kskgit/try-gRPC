e02a30e459768a9d4fb3ff618a6bc5c3939b2c04
keisuke <keisuke@diffeasy.com>
2021-02-16 07:41:24 +0900

    protoファイル作成

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/golang/protobuf/protoc-gen-go/grpc"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("faild to serve: %s", err)
+	}
+
+}
